2ad42275b455131fb04d4826acadacb3e135bfe4
glmsr12 <gulumsercakmak@hotmail.com>
2023-04-13 23:02:49 -0700

    "update files"

--- src/components/Cubicle.js
+++ src/components/Cubicle.js
@@ -1,6 +1,12 @@
 const Cubicle = ({ id, cubicle }) => {
   const handleClick = (e) => {
-    console.log(e.target);
+    const taken =
+      e.target.firstChild.classList.contains('circle') ||
+      e.target.firstChild.classList.contains('cross');
+
+      if(!taken) {
+        if (go)
+      }
   };
   return <div className="square" id={id} onClick={handleClick}></div>;
 };
